e1bdb4fc90f01e5cfa0e808fc1de1fccda36e858
Jar-box <jhervincerada@gmail.com>
2023-12-16 00:38:55 +0800

    Added post button on create post Added and modified shits regarding home and create post

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { TouchableOpacity, Text } from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -6,7 +7,7 @@ import HomeScreen from "./src/screens/HomeScreen";
 import JournalList from "./src/components/JournalList";
 import CreateJournalEntryScreen from "./src/screens/CreateJournalEntryScreen";
 import JournalDetailScreen from "./src/screens/JournalDetailScreen";
-import PostForm from "./src/components/PostForm";
+import CreatePost from "./src/components/CreatePost";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -64,8 +65,48 @@ const JournalStack = () => {
         name="JournalDetail"
         component={JournalDetailScreen}
         options={({ route }) => ({
-          // Use the note's date as the title
-          title: route.params.date, // Use the date as the title
+          title: route.params.date,
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ title: "Laiven" }}
+      />
+      <Stack.Screen
+        name="CreatePost"
+        component={CreatePost}
+        options={({ navigation }) => ({
+          title: "Create Post",
+          headerRight: () => (
+            <TouchableOpacity
+              style={{ marginRight: 16 }}
+              onPress={() => {
+                // Implement your logic for the "Post" button in Create Post
+                navigation.navigate("HomeScreen");
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: 700,
+                  color: "#F0F6F6",
+                  backgroundColor: "#CF22FF",
+                  borderRadius: 100,
+                  paddingVertical: 4,
+                  paddingHorizontal: 14,
+                }}
+              >
+                Post
+              </Text>
+            </TouchableOpacity>
+          ),
         })}
       />
     </Stack.Navigator>
@@ -77,16 +118,15 @@ const App = () => {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen
-          name="Journal"
-          component={() => <JournalStack />}
+          name="Home"
+          component={HomeStack}
           options={{ headerShown: false }}
         />
         <Tab.Screen
-          name="PostForm"
-          component={PostForm}
-          options={{ title: "Create Post" }}
+          name="Journal"
+          component={JournalStack}
+          options={{ headerShown: false }}
         />
-        <Tab.Screen name="Home" component={HomeScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
